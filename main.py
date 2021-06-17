fcabd40a116468991cf825acd2b209699ce9f312
Yukwok <billynyk@gmail.com>
2021-06-17 10:42:20 +0800

    buttons added

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
-print(f"main: \nquestion_data:\n{question_data}")
+# print(f"main: \nquestion_data:\n{question_data}")
 
 for question in question_data:
     question_text = question["question"]
@@ -13,11 +14,10 @@ for question in question_data:
 
 # testing
 quiz = QuizBrain(question_bank)
+ui = QuizInterface()
 
 while quiz.still_has_questions():
     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
-
-
--- ui.py
+++ ui.py
@@ -1,2 +1,35 @@
-THEME_COLOR = "#375362"
+from tkinter import *
+from PIL import ImageTk, Image
 
+
+THEME_COLOR = "#375362"
+PATH1 = "./images/false.png"
+PATH2 = "./images/true.png"
+
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score : 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150, 125,
+                                                     text="some question text",
+                                                     font=("Arial", 20, "italic"),
+                                                     fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        self.false_button_img = ImageTk.PhotoImage(Image.open(PATH1))
+        self.false_button = Button(image=self.false_button_img, highlightthickness=0)
+        self.false_button.grid(column=0, row=2)
+
+        self.true_button_img = ImageTk.PhotoImage(Image.open(PATH2))
+        self.true_button = Button(image=self.true_button_img, highlightthickness=0)
+        self.true_button.grid(column=1, row=2)
+
+        self.window.mainloop()
